e90de4c59d2ea9f3c8689fefbbba21d1fff362ab
MolodykhV <129886700+MolodykhV@users.noreply.github.com>
2023-04-12 20:26:08 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
-from statsmodels.stats.weightstats import ztest
+from statsmodels.stats import ttest_ind
 
 chat_id = 238786813 # Ваш chat ID, не меняйте название переменной
 
 def solution(control, test) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    zt = ztest(test, value = control.mean(), alternative='smaller')
+    zt = ttest_ind(test, control, alternative='less')
     return zt[1] < 0.01
