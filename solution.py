3c143a3e2a217739d134121969b8455e1a15fb36
MolodykhV <129886700+MolodykhV@users.noreply.github.com>
2023-04-13 18:45:08 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from statsmodels.stats import ttest_ind
+from scipy.stats import ttest_ind
 
 chat_id = 238786813 # Ваш chat ID, не меняйте название переменной
 
